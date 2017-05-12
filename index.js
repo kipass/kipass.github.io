7b7903680ffadff0d110cdeadd0ea9e9d64037bb
Kristopher Ives <kristopher.ives@gmail.com>
2017-05-12 03:30:33 -0700

    Added integrity check.

--- index.js
+++ index.js
@@ -3,39 +3,52 @@ var crypto = require('crypto');
 
 function kipass(cfg, f) {
   if (!cfg) { return f(new Error("Missing configuration")); }
+  if (typeof f !== 'function') { throw new Error("Missing callback"); }
   if (typeof cfg.masterPassword !== 'string') { return f(new Error("Missing master password")); }
   if (cfg.masterPassword.length < 6) { return f(new Error("Master password is too short")); }
   if (typeof cfg.domain !== 'string') { return f(new Error("Domain should be a string")); }
   if (cfg.domain.length <= 0) { return f(new Error("Domain is too short")); }
   cfg.length = Number(cfg.length);
   if (cfg.length <= 0 || isNaN(cfg.length)) { return f(new Error("Missing password length")); }
-  if (typeof cfg.alphabet !== 'string') { return f(new Error("Missing alphabet")); }
-  if (cfg.alphabet.length < 10) { return f(new Error("Password alphabet is too small")); }
+  if (!cfg.encoder && !Array.isArray(cfg.alphabets)) { return f(new Error("Missing any alphabets")); }
+  if (typeof cfg.condition !== 'function') { return f(new Error("Must pass a condition function")); }
 
-  var hashedPassword = sha512(cfg.masterPassword);
+  var seed = sha512(cfg.masterPassword);
   var hashedDomain = sha512(cfg.domain);
-  var salt = sha512(hashedPassword + hashedDomain);
-  var domainPassword = sha512(hashedPassword + masterPassword);
   var iterations = 1024 * 256;
 
-  crypto.pbkdf2(domainPassword, salt, iterations, cfg.length, 'sha256', function (err, key) {
+  function checkAttempt(err, key) {
+    var encoded;
+
     if (err) {
       f(err);
     } else if (key.length !== cfg.length) {
       f(new Error("Generated key is unexpected length"));
     } else {
-      f(false, encode(cfg, key));
+      encoded = encode(cfg, key);
+
+      if (cfg.condition(encoded, cfg)) {
+        f(false, encoded);
+      } else {
+        seed = sha512(seed);
+        attempt();
+      }
     }
-  });
+  }
+
+  function attempt() {
+    var salt = sha512(seed + hashedDomain);
+    var domainPassword = sha512(seed + cfg.masterPassword);
+    crypto.pbkdf2(domainPassword, salt, iterations, cfg.length, 'sha256', checkAttempt);
+  }
+
+  attempt();
 };
 
 function encode(cfg, key) {
-  var chars = [], pos = 0;
-
-  while (chars.length < cfg.length) {
-    chars.push(cfg.alphabet[(key[pos++] & 0xFF) % cfg.alphabet.length]);
-  }
-
+  if (cfg.encoder) { return cfg.encoder(key, cfg); }
+  var chars = [], alphabet = cfg.alphabets.join("");
+  for (var i=0; i < cfg.length; i++) { chars.push(alphabet[(key[i] & 0xFF) % alphabet.length]); }
   return chars.join("");
 }
 
@@ -44,7 +57,7 @@ function sha512(data) {
 }
 
 kipass.alphabets = {
-  numeric: "0123456789",
+  numbers: "0123456789",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   punctuation: "$.?!-_"
--- ui.js
+++ ui.js
@@ -3,16 +3,22 @@
 var kipass = require('./index.js');
 if (!kipass) { return alert("Failed to load kipass module"); }
 
+function reset() {
+  $('#domainPassword').val('');
+  $('#integrity *').text('');
+}
+
 function generate() {
   kipass({
     masterPassword: $('#masterPassword').val(),
     domain: $('#domain').val(),
     length: $('#passwordLength').val(),
-    alphabet: createAlphabet()
+    alphabets: getSelectedAlphabets(),
+    condition: hasEachAlphabet
   }, function (err, pass) {
     if (err) {
       alert("Unable to generate password because: " + String(err));
-      $('#domainPassword').val('');
+      reset();
       return;
     }
 
@@ -20,20 +26,103 @@ function generate() {
   });
 }
 
-function createAlphabet() {
-  return (
-     getAlphabet('#alphabet-uppercase', kipass.alphabets.uppercase) +
-     getAlphabet('#alphabet-lowercase', kipass.alphabets.lowercase) +
-     getAlphabet('#alphabet-numbers', kipass.alphabets.numbers) +
-     getAlphabet('#alphabet-punctuation', kipass.alphabets.punctuation)
-  );
+function checkIntegrity() {
+  reset();
+
+  if ($('#masterPassword').val().length <= 0) {
+    return;
+  }
+
+  var icons = {
+    cow: "🐄",
+    pig: "🐖",
+    goat: "🐐",
+    monkey: "🐒",
+    horse: "🐎",
+    snake: "🐍",
+    chicken: "🐔",
+    cat: "🐱",
+    dog: "🐶",
+    whale: "🐳"
+  };
+
+  kipass({
+    masterPassword: $('#masterPassword').val(),
+    domain: "kipass.github.io",
+    length: 2,
+    encoder: encodeIntegrity,
+    condition: function () { return true; }
+  }, function (err, integrity) {
+    if (err) {
+      alert("Unable to check integrity of password: " + String(err));
+      reset();
+      return;
+    }
+
+    var parts = integrity.split(' ');
+    var color = parts[0];
+    var animal = parts[1];
+    $('#integrity').css('color', color);
+    $('#integrity-text').text(integrity);
+    $('#integrity-icon').text(icons[animal]);
+  });
+}
+
+function encodeIntegrity(key) {
+  var colors = ["yellow", "purple", "orange", "black", "green", "pink", "red", "blue"];
+  var animals = ["pig", "goat", "chicken", "cow", "cat", "dog", "snake", "monkey", "horse", "whale"];
+  var x = (key[0] & 0xFF) % colors.length;
+  var y = (key[1] & 0xFF) % animals.length;
+  return colors[x] + " " + animals[y];
+}
+
+function hasEachAlphabet(pass, cfg) {
+  var lookup = {}, count = 0, c;
+
+  for (var i=0; i < pass.length; i++) {
+    c = pass[i];
+
+    for (var j=0; j < cfg.alphabets.length; j++) {
+      if (lookup[j]) { continue; }
+      lookup[j] = cfg.alphabets[j].indexOf(c) >= 0;
+      if (cfg.alphabets[j].indexOf(c) >= 0) { count++; lookup[j] = true; break; }
+    }
+
+    if (count === cfg.alphabets.length) { break; }
+  }
+
+  return count === cfg.alphabets.length;
+}
+
+function getSelectedAlphabets() {
+  return [
+     getAlphabet('#alphabet-uppercase', kipass.alphabets.uppercase),
+     getAlphabet('#alphabet-lowercase', kipass.alphabets.lowercase),
+     getAlphabet('#alphabet-numbers', kipass.alphabets.numbers),
+     getAlphabet('#alphabet-punctuation', kipass.alphabets.punctuation),
+     getAlphabet('#alphabet-custom', $('#alphabet-custom-input').val())
+  ].filter(function (x) { return (typeof x) === 'string'; });
 }
 
 function getAlphabet(selector, alphabet) {
-  return $(selector).is(':checked') ? alphabet : "";
+  return $(selector).is(':checked') ? alphabet : null;
 }
 
 $(function () {
+  $('#masterPassword')
+    .keydown(reset)
+    .change(checkIntegrity);
+
+  $('#showPassword').click(function () {
+    if ($('#masterPassword').attr('type') === 'text') {
+      $('#masterPassword').attr('type', 'password');
+      $(this).find('.glyphicon').removeClass('glyphicon-eye-open').addClass('glyphicon-eye-close');
+    } else {
+      $('#masterPassword').attr('type', 'text');
+      $(this).find('.glyphicon').removeClass('glyphicon-eye-close').addClass('glyphicon-eye-open');
+    }
+  });
+
   $('#generateButton').click(function () {
     $('#domainPassword').val('Generating...');
 
