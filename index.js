9c953b159c1556537376908272aef4f096f37c55
Kristopher Ives <kristopher.ives@gmail.com>
2017-05-14 07:04:18 -0700

    Fixed modulus when translating from byte to character.

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 
 var crypto = require('crypto');
 
+function isFunc(x) { return (typeof x) === 'function'; }
+function isString(x) { return (typeof x) === 'string'; }
+function isNumber(x) { return (typeof x) === 'number'; }
+function isArrayOrString(x) { return isString(x) || Array.isArray(x); }
+
 function kipass(cfg, f) {
-  if (typeof f !== 'function') { throw new Error("Missing callback"); }
+  if (!isFunc(f)) { throw new Error("Missing callback"); }
   if (!cfg) { return f(new Error("Missing configuration")); }
-  if (typeof cfg.masterPassword !== 'string') { return f(new Error("Missing master password")); }
+  if (!isString(cfg.masterPassword)) { return f(new Error("Missing master password")); }
   if (cfg.masterPassword.length < 6) { return f(new Error("Master password is too short")); }
-  if (typeof cfg.domain !== 'string') { return f(new Error("Domain should be a string")); }
+  if (!isString(cfg.domain)) { return f(new Error("Domain should be a string")); }
   if (cfg.domain.length <= 0) { return f(new Error("Domain is too short")); }
   cfg.length = Number(cfg.length);
   if (cfg.length <= 0 || isNaN(cfg.length)) { return f(new Error("Missing password length")); }
-  if (!cfg.encoder && !Array.isArray(cfg.alphabets)) { return f(new Error("Missing any alphabets")); }
-  if (typeof cfg.condition !== 'function') { return f(new Error("Must pass a condition function")); }
+  if (!Array.isArray(cfg.alphabets) && !isFunc(cfg.formatter)) { return f(new Error("Missing any alphabets")); }
+  if (!isFunc(cfg.condition)) { return f(new Error("Must pass a condition function")); }
 
   var seed = sha512(cfg.masterPassword);
   var hashedDomain = sha512(cfg.domain);
   var iterations = 1024 * 256;
 
   function checkAttempt(err, key) {
-    var encoded;
-
     if (err) {
       f(err);
     } else if (key.length !== cfg.length) {
       f(new Error("Generated key is unexpected length"));
     } else {
-      encoded = encode(cfg, key);
+      acceptAttempt(key);
+    }
+  }
 
-      if (cfg.condition(encoded, cfg)) {
-        f(false, encoded);
-      } else {
-        seed = sha512(seed);
-        attempt();
-      }
+  function acceptAttempt(key) {
+    var translated = translate(cfg, key);
+
+    if (cfg.condition(translated, cfg)) {
+      f(false, translated);
+    } else {
+      seed = sha512(seed);
+      attempt();
     }
   }
 
@@ -43,14 +50,23 @@ function kipass(cfg, f) {
   }
 
   attempt();
-};
+}
 
-function encode(cfg, key) {
-  if (cfg.encoder) { return cfg.encoder(key, cfg); }
+function translate(cfg, key) {
+  if (cfg.formatter) { return cfg.formatter(key, cfg); }
   var chars = [], alphabet = cfg.alphabets.join("");
-  for (var i=0; i < cfg.length; i++) { chars.push(alphabet[(key[i] & 0xFF) % alphabet.length]); }
+  for (var i=0; i < cfg.length; i++) { chars.push(translateByte(alphabet, key[i])); }
   return chars.join("");
 }
+
+function translateByte(alphabet, value) {
+  if (!isArrayOrString(alphabet)) { throw new Error("Alphabet must be a string or array"); }
+  if (!isNumber(value)) { throw new Error("Expected a number to turn into a char"); }
+  if (value < 0 || value > 255) { throw new Error("Value is outside expected range"); }
+  return alphabet[Math.round((value / 255.0) * (alphabet.length - 1))];
+}
+
+kipass.translateByte = translateByte;
 
 function sha512(data) {
   return crypto.createHash('sha512').update(data, 'utf8').digest();
@@ -60,7 +76,7 @@ kipass.alphabets = {
   numbers: "0123456789",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  punctuation: "$.?!-_"
+  punctuation: "!$-.?_"
 };
 
 module.exports = kipass;
